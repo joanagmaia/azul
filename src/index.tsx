e24c08f668e4817eb4cfd32049c1b6a4652a7360
Joana Maia <joana.maia@talkdesk.com>
2021-10-21 18:18:48 +0100

    chore: Setup application with typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
+import App from 'components/App';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-    <h1>Hello World</h1>,
+  <App />,
   document.getElementById('root')
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CheckerPlugin } = require('awesome-typescript-loader')
+
 
 module.exports = {
-  entry: path.join(__dirname, "src", "index.js"),
+  entry: path.join(__dirname, "src", "index.tsx"),
   output: {
     path:path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+    modules: [path.resolve(__dirname, './src'), 'node_modules'],
   },
   module: {
     rules: [
@@ -14,15 +20,20 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
+            presets: ['@babel/preset-env', '@babel/preset-react', "@babel/typescript"],
           }
         }
       },
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader'
+      }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src", "index.html"),
+      template: path.join(__dirname, "public", "index.html"),
     }),
+    new CheckerPlugin(),
   ],
 }
